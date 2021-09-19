f1a0ad345ecc3941133049b34d6429a21c5abb02
Borshchick <19archenemy92@gmail.com>
2021-09-19 23:56:26 +0300

    add logic to reducer

--- src/main/bll/login-reducer.ts
+++ src/main/bll/login-reducer.ts
@@ -1,16 +1,72 @@
-const initialState = {}
+import {ThunkType} from "./store"
+import {herokuAPI, LoginParamsType, LoginResponseType, ResponseErrorType} from "../../api/Api"
+
+const initialState = {
+    _id: "",
+    email: "",
+    name: "",
+    avatar: "",
+    publicCardPacksCount: 0,
+    isAdmin: false,
+    error: "",
+}
 
 export const loginReducer = (state: LoginStateType = initialState, action: ActionTypes): LoginStateType => {
     switch (action.type) {
+        case "LOGIN_REDUCER___LOGIN_PAGE": {
+            return {
+                ...state,
+                ...action.loginData
+            }
+        }
+        case "LOGIN_REDUCER___SET_ERROR": {
+            return {
+                ...state,
+                error: action.error
+            }
+        }
         default:
-            return state;
+            return state
     }
 }
 
 //Actions
-export const ActionCreator = () => ({type: 'ACTION'});
+export const loginAC = (loginData: LoginStateType) => ({
+        type: "LOGIN_REDUCER___LOGIN_PAGE",
+        loginData
+    } as const
+)
+
+export const setError = (error: string) => {
+    return {
+        type: "LOGIN_REDUCER___SET_ERROR",
+        error
+    } as const
+}
+
+export const login = (data: LoginParamsType): ThunkType =>
+    async (dispatch) => {
+        const res: LoginResponseType & ResponseErrorType = await herokuAPI.authLogin(data)
+        if (res.error) {
+            dispatch(setError(res.error ? res.error: "Something wrong"))
+        }
+        dispatch(loginAC(res))
+    }
 
 //Types
-type LoginStateType = typeof initialState;
+//export type LoginStateType = typeof initialState;
+export type LoginStateType = {
+    _id: string
+    email: string
+    name: string
+    avatar?: string
+    publicCardPacksCount: number
+    isAdmin: boolean
+    error?: string
+};
 
-type ActionTypes = ReturnType<typeof ActionCreator>
+type LoginACType = ReturnType<typeof loginAC>
+type SetErrorType = ReturnType<typeof setError>
+
+export type ActionTypes = LoginACType | SetErrorType
+
